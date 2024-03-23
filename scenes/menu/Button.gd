4e80e7dfd438dfff5afc7d438cffc5e56a974aeb
William Dumoulin <william@idt.be>
2024-03-23 23:48:20 +0100

    Images for Menu
    Images et nouvelle police

--- scenes/menu/Button.gd
+++ scenes/menu/Button.gd
@@ -1,8 +1,13 @@
 extends Button
 
-@onready var button_fx = preload("res://assets/Sound/Sound_effect/HUD_bouton_presse1.wav")
+@onready var button_pressed_fx = preload("res://assets/Sound/Sound_effect/HUD/HUD_bouton_presse1.wav")
+@onready var button_hovered_fx = preload("res://assets/Sound/Sound_effect/HUD/HUD_bouton_presse3-Survole.wav")
 
 
 func _on_pressed():
-	AudioPlayer.play_fx(button_fx)
+	AudioPlayer.play_fx(button_pressed_fx)
 
+
+
+func _on_mouse_entered():
+	AudioPlayer.play_fx(button_hovered_fx)
