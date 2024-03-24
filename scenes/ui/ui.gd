a9822d0e401d62fc88d4e00871decb9ddd7797ce
William Dumoulin <william@idt.be>
2024-03-24 08:16:35 +0100

    dead and restart + sound

--- scenes/ui/ui.gd
+++ scenes/ui/ui.gd
@@ -1,6 +1,8 @@
 extends CanvasLayer
 
 @onready var sand_storm = $sand_storm
+@onready var game_over = get_node("GameOver")
+@onready var dead_fx = preload("res://assets/Sound/Sound_effect/Game/Bruit_dead.wav")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,3 +13,12 @@ func enable_sand_storm():
 
 func disable_sand_storm():
 	sand_storm.visible = false
+
+
+func _on_player_dead():
+	#Add Game Over layer
+	game_over.visible = true
+	#disable mvt -> Done by removing the player
+	#fx
+	AudioPlayer.play_fx(dead_fx)
+	pass # Replace with function body.
